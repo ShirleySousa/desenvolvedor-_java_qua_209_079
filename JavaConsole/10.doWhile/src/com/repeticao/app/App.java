1164770e78160d6af2fc307620e26d846a488561
ShirleySousa <shirleyasousa@hotmail.com>
2025-08-07 17:44:51 -0300

    Aula 07/08/2025

--- JavaConsole/10.doWhile/src/com/repeticao/app/App.java
+++ JavaConsole/10.doWhile/src/com/repeticao/app/App.java
@@ -46,3 +46,5 @@ public class App {
         leia.close();
     }
 }
+
+
--- atividades/atividade01/src/com/atividade01/app/App.java
+++ atividades/atividade01/src/com/atividade01/app/App.java
@@ -2,6 +2,7 @@ package com.atividade01.app;
 
 import java.util.Scanner;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner leia = new Scanner(System.in);
@@ -10,36 +11,69 @@ public class App {
         int opcao;
         double result, altura, peso;
 
-        // faça...enquanto
-        System.out.println("1 Registra nova entrada");
-        System.out.println("2 - Sair do programa.");
-        System.out.println("unfrme  opção desejada:");
+        do {
+            System.out.println("1 - Registra nome usuario.");
+            System.out.println("2 - Sair do programa.");
+            System.out.println("Informe a opção desejada:");
+            opcao = leia.nextInt();
+        
 
-        if (opcao == 1) {
+            if (opcao == 1) {
+        
             leia.nextLine();
             System.out.println("informe o nome:");
             name = leia.nextLine();
-            System.out.println("Informe o peso");
+            System.out.println("Informe o peso:");
             peso = leia.nextInt();
-            System.out.println("informe a altura");
+            System.out.println("informe a altura:");
             altura = leia.nextDouble();
-            
 
             // calcular IMX
             result = peso / (altura * altura);
+        
 
             // Verificar IMC
 
-            if (result >= 16) {
-                System.out.println("o seu IMC é de " + result);
-
-            } else {
-                System.out.println(name + "não esta autrizado");
-
-                leia.close();
-
+            if (result <= 16) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " magreza grave.");
+            }
+            if (result > 16.11 && result < 17) {
+                System.out.println(" o seu IMC é de " +String.format("%.2f" , result) + " magreza moderada.");
             }
 
+            if (result > 17.1 && result < 18.5) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " magreza leve.");
+            }
+
+            if (result > 18.51 && result < 25) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " está saudável.");
+            }
+
+            if (result > 25.1 && result < 30) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " está com sob peso.");
+            }
+
+            if (result > 30.1 && result < 35) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " está com obsidade grau 1.");
+            }
+            if (result > 35.1 && result < 40) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " está com obsidade grau 2.");
+            }
+            if (result > 40.1) {
+                System.out.println(" o seu IMC é de " + String.format("%.2f" , result) + " está com obsidade grau 3.");
+            }
         }
-    }
-}
+
+        
+        } while (opcao != 2);
+        leia.close();
+ 
+         }
+         
+        }
+    
+       
+        
+         
+         
+
