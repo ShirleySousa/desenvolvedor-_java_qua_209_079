557d346953f923a6021addf8d367f9ef5a805bb9
ShirleySousa <shirleyasousa@hotmail.com>
2025-08-11 17:47:04 -0300

    Aula POO

---
+++ atividades/atividade.03/com.atividade.app/src/com/atividade/app/App.java
@@ -0,0 +1,20 @@
+package com.atividade.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO
+        /*
+         * crie um programa com uma classe chamada Conta, com os seguintes atributos:
+         * + Titular da conta
+         * -CPF do tituar
+         * Agencia
+         * numero da conta
+         * saldo
+         * o usuario devera informar o nome do titular e o cpf, e o programa exibe um menu com as seguintes operações
+         * exibir dados da conta
+         * fazer saque
+         * fazer depósito
+         * sair do programa 
+         */
+
+    }
+}
