e2456b8095ca957a6812a844fded48dab8e12bba
ShirleySousa <shirleyasousa@hotmail.com>
2025-09-10 17:11:16 -0300

    String com formulario

---
+++ spring/telas/telas/telas/src/main/java/com/telas/app/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.telas.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class IndexController {
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @RequestMapping("/formulario")
+  public String formulario() {
+    return "formulario";
+  }
+
+}
